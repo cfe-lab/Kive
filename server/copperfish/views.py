592b9bf27dfd38569b6530d88b3dfbe44d48d7d1
Art Poon <artpoon@gmail.com>
2013-10-28 16:30:48 -0700

    detabbing and docstrings in views.py

--- server/copperfish/views.py
+++ server/copperfish/views.py
@@ -7,25 +7,30 @@ from copperfish.forms import DatatypeForm
 from django.core.context_processors import csrf
 
 def home(request):
-	t = loader.get_template('index.html')
-	c = Context()
-	return HttpResponse(t.render(c))
+    """
+    Default homepage
+    """
+    t = loader.get_template('index.html')
+    c = Context()
+    return HttpResponse(t.render(c))
 
 
 def datatypes(request):
-	#return HttpResponse(t.render(c))
-	
-	if request.method == 'POST':
-		form = DatatypeForm(request.POST)
-		if form.is_valid():
-			new_datatype = form.save()
-	else:
-		form = DatatypeForm() # unbound
-	
-	datatypes = models.Datatype.objects.all()
-	t = loader.get_template('datatypes.html')
-	c = Context({'datatypes': datatypes, 'form': form})
-	c.update(csrf(request))
-	
-	return HttpResponse(t.render(c))
-	#return render_to_response('datatypes.html', {'form': form})
+    """
+    Render table and form on user request for datatypes.html
+    """
+
+    if request.method == 'POST':
+        form = DatatypeForm(request.POST)
+        if form.is_valid():
+            new_datatype = form.save()
+    else:
+        form = DatatypeForm() # unbound
+
+    datatypes = models.Datatype.objects.all()
+    t = loader.get_template('datatypes.html')
+    c = Context({'datatypes': datatypes, 'form': form})
+    c.update(csrf(request))
+
+    return HttpResponse(t.render(c))
+    #return render_to_response('datatypes.html', {'form': form})
