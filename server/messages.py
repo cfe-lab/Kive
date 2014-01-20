086d2d83673e102063501d293b60b4bbbc30e329
Rosemary McCloskey <rmccloskey@cfenet.ubc.ca>
2014-01-20 15:28:02 -0800

    Tiny changes.

--- server/messages.py
+++ server/messages.py
@@ -6,5 +6,7 @@ error_messages = {
         'Method "{}" cannot have CodeResourceRevision "{}" as a driver, because it has no content file.',
     "transf_noinput": 'Transformation "{}" has no inputs.',
     "method_bad_inputcount": 
-        'Method "{}" expects {} inputs and {} outputs, but {} inputs and {} outputs were supplied'
+        'Method "{}" expects {} inputs and {} outputs, but {} inputs and {} outputs were supplied',
+    "pipeline_bad_inputcount":
+        'Pipeline "{}" expects {} inputs, but {} were supplied'
 }
