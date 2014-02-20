16ce2327ac1010fac8a5b6cae9d42fdcbd8c2c2c
ArtPoon <artpoon@gmail.com>
2014-02-19 23:45:26 -0800

    preparing to add user interface for uploading and browsing Datasets; HTML5 Canvas interface for pipeline assembly

--- shipyard/librarian/views.py
+++ shipyard/librarian/views.py
@@ -1,1 +1,7 @@
-# Create your views here.
+"""
+librarian views
+"""
+
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import loader, Context
+from django.core.context_processors import csrf
--- shipyard/pipeline/views.py
+++ shipyard/pipeline/views.py
@@ -1,1 +1,19 @@
-# Create your views here.
+"""
+pipeline views
+"""
+
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import loader, Context
+from django.core.context_processors import csrf
+from method.models import Method, MethodFamily
+
+def pipelines(request):
+    """
+    Display a list of all code resources (parents) in database
+    """
+    t = loader.get_template('pipeline/pipelines.html')
+    method_families = MethodFamily.objects.all()
+    methods = Method.objects.all()
+    c = Context({'method_families': method_families, 'methods': methods})
+    c.update(csrf(request))
+    return HttpResponse(t.render(c))
