ca4e4f71d0e2be1b269aae8b6f7b4ea6c63d0054
ArtPoon <artpoon@gmail.com>
2014-03-21 15:31:41 -0700

    forgot to add files to previous commit

---
+++ shipyard/pipeline/forms.py
@@ -0,0 +1,21 @@
+"""
+Forms for creating Pipeline objects.
+"""
+
+from django import forms
+from pipeline.models import PipelineStep
+
+class PipelineStepForm (forms.ModelForm):
+    """
+    content_type - either 'method' or 'pipeline'
+    object_id - ???
+    transformation - ForeignKey to Method or Pipeline
+    step_num - the step this transformation occupies in Pipeline
+    outputs_to_delete - keys to TransformationOutput objects
+    """
+    class Meta:
+        model = PipelineStep
+        exclude = (pipeline, )
+
+class PipelineStepInputCableForm (forms.ModelForm):
+    pass
