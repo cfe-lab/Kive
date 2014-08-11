ed25e2184ee6943c40ca4c927625b4be3ada5db4
Rosemary <rmccloskey@cfenet.ubc.ca>
2014-08-11 12:38:42 -0700

    Added view link on datasets page (closes #181).

---
+++ shipyard/archive/templatetags/filters.py
@@ -0,0 +1,10 @@
+from django import template
+from django.template.defaultfilters import stringfilter
+
+register = template.Library()
+
+@register.filter
+@stringfilter
+def get_view_link(download_link):
+    """Transform a download link into the equivalent view link."""
+    return download_link.replace("download", "view")
