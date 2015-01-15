bf9ac4520a14557392aa4caf0fb697eda64a905f
Richard Liang <rliang@cfenet.ubc.ca>
2015-01-15 14:15:50 -0800

    Removed deprecated nukeDB.bash script and its derivates.

--- shipyard/nukeForDemo.bash
+++
@@ -1,3 +0,0 @@
-./nukeDB.bash
-python2.7 manage.py loaddata demo
-cp -R FixtureFiles/demo/* .
--- shipyard/nukeSimple.bash
+++
@@ -1,4 +0,0 @@
-export DJANGO_SETTINGS_MODULE=shipyard.settings
-./nukeDB.bash
-python2.7 manage.py loaddata initial_pipeline
-python2.7 setup_test_pipeline.py
