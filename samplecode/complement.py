9c51c709aef9a38226e64c5f9cc732aaf0fbb835
Richard Liang <rliang@cfenet.ubc.ca>
2015-02-25 17:37:34 -0800

    Small changes to spacing in a piece of sample code.

--- samplecode/complement.py
+++ samplecode/complement.py
@@ -5,30 +5,29 @@
 The specified input CSV file should have one column (with header) consisting
 of valid RNA/DNA sequences."""
 
-from Bio.Seq import Seq;
-import argparse;
-import csv;
-import sys;
+from Bio.Seq import Seq
+import argparse
+import csv
+import sys
 
 parser = argparse.ArgumentParser("Complements the given RNA/DNA sequences")
 parser.add_argument("seqs_to_complement",
-                    help = "One-column CSV of RNA/DNA sequences")
-parser.add_arguments("output_csv_1",
-					help = "Complemented CSV")
-args = parser.parse_args();
+                    help="One-column CSV of RNA/DNA sequences")
+parser.add_arguments("output_csv_1", help="Complemented CSV")
+args = parser.parse_args()
 
-header = "";
-complemented = [];
+header = ""
+complemented = []
 
 with open(args.seqs_to_complement, "rb") as f:
-	seq_reader = csv.reader(f);
-	header = seq_reader.next();
-	for row in seq_reader:
-		complemented += [(Seq(row[0]).complement().tostring(),)];
+    seq_reader = csv.reader(f)
+    header = seq_reader.next()
+    for row in seq_reader:
+        complemented += [(Seq(row[0]).complement().tostring(),)]
 
 with open(args.output_csv_1, "wb") as f:
-	seq_writer = csv.writer(f);
-	seq_writer.writerow(("ComplementedSeq",));
-	seq_writer.writerows(complemented);
+    seq_writer = csv.writer(f)
+    seq_writer.writerow(("ComplementedSeq",))
+    seq_writer.writerows(complemented)
 
-sys.exit(0);
+sys.exit(0)
