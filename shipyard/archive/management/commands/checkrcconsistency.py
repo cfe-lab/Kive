447ee2b44f50f229428c5644d0524d030e7a8b0b
Joshua Horacsek <joshua_horacsek@sfu.ca>
2015-04-15 10:04:41 -0700

    Fixing issue #295, a management command 'checkrcconsistency' now exists that will check and optionally fix any inconsistencies found between the status flags for RunComponents (_succesful, _complete) and their actual calculated values.

--- shipyard/archive/management/commands/checkrcconsistency.py
+++ shipyard/archive/management/commands/checkrcconsistency.py
@@ -14,27 +14,18 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         fix_inconsistencies = options['fix_inconsistencies']
 
-        # We only use the _completed/_successful flags on
-        # runsteps and runcables.
+        # We only use the _completed/_successful flags on runsteps and runcables.
         # TODO: Filter out redacted steps once that field has been added
         for rc in chain(RunStep.objects.all(), RunCable.objects.all()):
-            inconsistent = False
-
-            is_compl, is_mcompl = rc.is_complete(), rc.is_marked_complete()
-            is_succs, is_msuccs = rc.is_successful(), rc.is_marked_successful()
+            is_mcompl, is_compl = rc.is_marked_complete(), rc.is_complete(dont_save=not fix_inconsistencies)
+            is_msuccs, is_succs = rc.is_marked_successful(), rc.is_successful(dont_save=not fix_inconsistencies)
 
             if is_compl != is_mcompl:
                 self.stdout.write('%s %s (id: %d) is_complete has mark(%s) but was computed to be (%s) ' % (
                     type(rc).__name__, rc, rc.id, is_compl, is_mcompl))
-                inconsistent = True
 
             if is_succs != is_msuccs:
                 self.stdout.write('%s %s (id: %d) is_successful has mark(%s) but was computed to be (%s) ' % (
                     type(rc).__name__, rc, rc.id, is_succs, is_msuccs))
-                inconsistent = True
-
-            # Saving will recompute these flags if inconsistent
-            if fix_inconsistencies and inconsistent:
-                rc.save()
 
         self.stdout.write('Completed check!')
