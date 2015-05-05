db183e5c4c2058e96b02368dc683515b03f46816
don <donkirkby@gmail.com>
2015-05-05 12:22:59 -0700

    Extract helper classes from CompoundDatatypeViewSet for issue #300.

--- kive/metadata/ajax.py
+++ kive/metadata/ajax.py
@@ -5,15 +5,12 @@ Handle Ajax transaction requests from metadata templates.
 from django.http import HttpResponse
 from django.core import serializers
 from django.contrib.auth.decorators import login_required, user_passes_test
-from rest_framework import viewsets, permissions, mixins
-from rest_framework.decorators import detail_route
-from rest_framework.response import Response
+from rest_framework import permissions
 
-from metadata.models import Datatype, get_builtin_types, CompoundDatatype,\
-    AccessControl
+from kive.ajax import IsDeveloperOrGrantedReadOnly, RemovableModelViewSet
+from metadata.models import Datatype, get_builtin_types, CompoundDatatype
 from metadata.serializers import CompoundDatatypeSerializer
-from portal.views import developer_check, admin_check
-from portal.ajax import IsDeveloperOrGrantedReadOnly
+from portal.views import developer_check
 
 @login_required
 @user_passes_test(developer_check)
@@ -38,8 +35,7 @@ def get_python_type(request):
         response.write(serializers.serialize("json", python_types, fields=('pk', 'name')))
         return response
     
-class CompoundDatatypeViewSet(mixins.DestroyModelMixin,
-                              viewsets.ReadOnlyModelViewSet):
+class CompoundDatatypeViewSet(RemovableModelViewSet):
     """ Compound datatypes are used to define a CSV file format.
     
     Query parameters for the list view:
@@ -51,21 +47,3 @@ class CompoundDatatypeViewSet(mixins.DestroyModelMixin,
     queryset = CompoundDatatype.objects.all()
     serializer_class = CompoundDatatypeSerializer
     permission_classes = (permissions.IsAuthenticated, IsDeveloperOrGrantedReadOnly)
-
-    def get_queryset(self):
-        if self.request.QUERY_PARAMS.get('is_granted') == 'true':
-            is_admin = False
-        else:
-            is_admin = admin_check(self.request.user)
-        return AccessControl.filter_by_user(self.request.user,
-                                            is_admin=is_admin,
-                                            queryset=self.queryset)
-
-    @detail_route(methods=['get'])
-    def removal_plan(self, request, pk=None):
-        removal_plan = self.get_object().build_removal_plan()
-        counts = {key: len(targets) for key, targets in removal_plan.iteritems()}
-        return Response(counts)
-    
-    def perform_destroy(self, instance):
-        instance.remove()
