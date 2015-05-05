46b388d0c9bbaaafdda7c074bab8d5c9af9e4e5f
don <donkirkby@gmail.com>
2015-05-04 17:12:42 -0700

    Switch from is_admin flag to a separate '/granted' URL for issue #300.

--- kive/metadata/ajax.py
+++ kive/metadata/ajax.py
@@ -6,10 +6,11 @@ from django.http import HttpResponse
 from django.core import serializers
 from django.contrib.auth.decorators import login_required, user_passes_test
 from rest_framework import viewsets, permissions, mixins
-from rest_framework.decorators import detail_route
+from rest_framework.decorators import detail_route, list_route
 from rest_framework.response import Response
 
-from metadata.models import Datatype, get_builtin_types, CompoundDatatype
+from metadata.models import Datatype, get_builtin_types, CompoundDatatype,\
+    AccessControl
 from metadata.serializers import CompoundDatatypeSerializer
 from portal.views import developer_check
 from portal.ajax import IsDeveloperOrGrantedReadOnly
@@ -43,18 +44,26 @@ class CompoundDatatypeViewSet(mixins.DestroyModelMixin,
     serializer_class = CompoundDatatypeSerializer
     permission_classes = (permissions.IsAuthenticated, IsDeveloperOrGrantedReadOnly)
 
-    def get_queryset(self):
-        is_admin = self.request.QUERY_PARAMS.get('is_admin') == 'true'
-        return CompoundDatatype.filter_by_user(self.request.user,
-                                               is_admin=is_admin,
-                                               queryset=self.queryset)
-
     @detail_route(methods=['get'])
     def removal_plan(self, request, pk=None):
-        compounddatatype = self.get_object()
-        removal_plan = compounddatatype.build_removal_plan()
+        removal_plan = self.get_object().build_removal_plan()
         counts = {key: len(targets) for key, targets in removal_plan.iteritems()}
         return Response(counts)
+    
+    @list_route(methods=['get'])
+    def granted(self, request):
+        queryset = AccessControl.filter_by_user(
+            request.user,
+            False,
+            self.filter_queryset(self.get_queryset()))
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
     
     def perform_destroy(self, instance):
         instance.remove()
