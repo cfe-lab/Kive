811dca0842b51887997e334cc0adad585fa34577
Art Poon <artpoon@gmail.com>
2015-06-10 16:30:59 -0700

    fixed bugs in CodeResourceRevision summary page, broken back button when revising CR

--- kive/method/static/method/resource_revisions.js
+++ kive/method/static/method/resource_revisions.js
@@ -4,12 +4,12 @@
 (function(window){
 
     function coderevision_link($td, revision) {
-        var $a = $("<a/>").attr("href", revision["absolute_url"]).text(revision["display_name"]);
+        var $a = $("<a/>").attr("href", revision["absolute_url"]).text("Revise");
         $td.append($a);
     }
 
     function coderevision_view_link($td, revision) {
-        var $a = $("<a/>").attr("href", revision["view_url"]).text("Revise");
+        var $a = $("<a/>").attr("href", revision["view_url"]).text(revision["display_name"]);
         $td.append($a);
     }
 
@@ -17,8 +17,8 @@
         permissions.PermissionsTable.call(this, $table, is_user_admin);
         this.list_url = "../../api/coderesources/" + ccr_pk + "/revisions/";
         this.registerColumn("#", "revision_number");
-        this.registerColumn("Name", coderevision_link);
-        this.registerColumn("", coderevision_view_link);
+        this.registerColumn("Name", coderevision_view_link);
+        this.registerColumn("", coderevision_link);
         this.registerColumn("Description", "revision_desc");
         this.registerColumn("Date", "revision_DateTime");
     };
