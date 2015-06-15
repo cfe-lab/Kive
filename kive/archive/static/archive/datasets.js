23bfee40285096904919536a7dc2ca35743c33d0
don <donkirkby@gmail.com>
2015-06-15 10:52:50 -0700

    Enable JSHint on all Javascript in archive app, and fix some complaints.
    Also add a test for Javascript MD5, and remove some code that was never
    run. (MD5 of 'hello' isn't long enough to call add32().)

--- kive/archive/static/archive/datasets.js
+++ kive/archive/static/archive/datasets.js
@@ -2,46 +2,46 @@ var datasets = (function() {
     "use strict";
     var my = {};
 
-function buildDateCreated($td, row) {
-    $td.text(permissions.formatDate(row.date_created));
-}
-
-function buildDownload($td, dataset) {
-    $td.append($('<a/>').text('Download').attr('href', dataset.download_url));
-}
-
-function buildDescription($td, dataset) {
-    $.each(dataset.description.split('\n'), function(_, txt){
-        $td.append(txt);
-        $td.append($('<br/>'))
-    });
-}
-
-var DatasetsTable = function($table, is_user_admin) {
-    permissions.PermissionsTable.call(this, $table, is_user_admin);
-    this.list_url = "/api/datasets/";
-    this.registerColumn("Name", function($td, dataset) {
-        $td.append($('<a/>')
-                .text(dataset.name).attr('href', '/dataset_view/'+dataset.id));
-    });
-    this.registerColumn("Description", buildDescription);
-    this.registerColumn("Date Created", buildDateCreated);
-    this.registerColumn("File Size (B)", "filesize");
-    this.registerColumn("", buildDownload);
-}
-DatasetsTable.prototype = Object.create(
-        permissions.PermissionsTable.prototype);
-DatasetsTable.prototype.getQueryParams = function() {
-    var params = permissions.PermissionsTable.prototype.getQueryParams.call(this);
-    params.is_uploaded = true;
-    return params;
-}
-
-my.main = function(is_user_admin, bootstrap) {
-    noXSS();
-    var table = new DatasetsTable($('#datasets'), is_user_admin);
-    table.buildTable(bootstrap);
-};
+    function buildDateCreated($td, row) {
+        $td.text(permissions.formatDate(row.date_created));
+    }
+    
+    function buildDownload($td, dataset) {
+        $td.append($('<a/>').text('Download').attr('href', dataset.download_url));
+    }
+    
+    function buildDescription($td, dataset) {
+        $.each(dataset.description.split('\n'), function(_, txt){
+            $td.append(txt);
+            $td.append($('<br/>'));
+        });
+    }
+    
+    var DatasetsTable = function($table, is_user_admin) {
+        permissions.PermissionsTable.call(this, $table, is_user_admin);
+        this.list_url = "/api/datasets/";
+        this.registerColumn("Name", function($td, dataset) {
+            $td.append($('<a/>')
+                    .text(dataset.name).attr('href', '/dataset_view/'+dataset.id));
+        });
+        this.registerColumn("Description", buildDescription);
+        this.registerColumn("Date Created", buildDateCreated);
+        this.registerColumn("File Size (B)", "filesize");
+        this.registerColumn("", buildDownload);
+    };
+    DatasetsTable.prototype = Object.create(
+            permissions.PermissionsTable.prototype);
+    DatasetsTable.prototype.getQueryParams = function() {
+        var params = permissions.PermissionsTable.prototype.getQueryParams.call(this);
+        params.is_uploaded = true;
+        return params;
+    };
+    
+    my.main = function(is_user_admin, bootstrap) {
+        noXSS();
+        var table = new DatasetsTable($('#datasets'), is_user_admin);
+        table.buildTable(bootstrap);
+    };
 
     return my;
 }());
