8b028888f22e09db8a43120f2eba8793c06cf1aa
Richard Liang <rliang@cfenet.ubc.ca>
2015-06-22 11:20:02 -0700

    Forgot to commit the initial migration in the portal app.

---
+++ kive/portal/migrations/0001_initial.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='StagedFile',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('uploaded_file', models.FileField(help_text=b'Uploaded file held for further server-side processing', upload_to=b'StagedFiles', verbose_name=b'Uploaded file')),
+                ('date_uploaded', models.DateTimeField(help_text=b'Date and time of upload', verbose_name=b'Upload date', auto_now_add=True)),
+                ('user', models.ForeignKey(help_text=b'User that uploaded this file', to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
