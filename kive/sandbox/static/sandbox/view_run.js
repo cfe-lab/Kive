daab579df6cf4705f01234bb6531ddd6bf4b3352
Richard Liang <rliang@cfenet.ubc.ca>
2015-07-10 12:58:14 -0700

    Fixed a small bug where the View Results link from the View Runs page had the wrong key.

--- kive/sandbox/static/sandbox/view_run.js
+++ kive/sandbox/static/sandbox/view_run.js
@@ -27,7 +27,7 @@ function setupRunView(rtp_id, pipeline_id, md5) {
                 $msg = $('<span class="status-message">');
             $msg.append($('<a>Complete</a>').attr(
                     'href',
-                    '/view_results/' + parseInt(run.id)));
+                    '/view_results/' + parseInt(rtp_id)));
 
             // TODO: Use a better system of reporting overall run status
             if(stat.indexOf('?') >= 0) {
@@ -35,7 +35,7 @@ function setupRunView(rtp_id, pipeline_id, md5) {
                         $('<img src="/static/sandbox/preload.gif"/>'));
             }
             else if (stat.indexOf('Too') >= 0) {
-                $msg.empty().text(stat)
+                $msg.empty().text(stat);
                 clearInterval(self.timer);
             }
             else {
@@ -48,7 +48,7 @@ function setupRunView(rtp_id, pipeline_id, md5) {
                     $msg.empty().append($('<a>In progress </a> &nbsp;').attr(
                             'href',
                             '/view_results/' + rtp_id +'?back_to_view=true')).append(
-                            $('<img src="/static/sandbox/preload.gif"/>'))
+                            $('<img src="/static/sandbox/preload.gif"/>'));
                 } else {
                     clearInterval(self.timer);
                 }
