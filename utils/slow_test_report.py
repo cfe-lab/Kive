e0317efd7c86be93fc6ad8bbad81240699ea7d77
don <donkirkby@gmail.com>
2015-10-13 09:07:17 -0700

    Update slow test report with new baseline for issue #296.

--- utils/slow_test_report.py
+++ utils/slow_test_report.py
@@ -4,18 +4,18 @@ from operator import attrgetter
 import bisect
 import glob
 
-# To track progress, here is how we started on Dec 31, against SQLite:
-# 10 out of 704 tests took 136s out of 499s
-# 17s for archive.GetCoordinatesTests.test_get_coordinates_nested_roc
-# 17s for archive.GetCoordinatesTests.test_get_coordinates_nested_rsic
-# 17s for archive.GetCoordinatesTests.test_get_coordinates_nested_runstep
-# 17s for archive.GetCoordinatesTests.test_get_coordinates_nested_runs
-# 17s for archive.TopLevelRunTests.test_deep_nested_run
-# 15s for sandbox.SandboxTests.test_pipeline_execute_C_twostep_pipeline_with_subpipeline
-# 14s for sandbox.ExecuteTests.test_pipeline_execute_C_twostep_pipeline_with_subpipeline
-# 8s for sandbox.SandboxTests.test_pipeline_execute_B_twostep_pipeline_with_recycling
-# 7s for sandbox.ExecuteTests.test_pipeline_execute_B_twostep_pipeline_with_recycling
+# To track progress, here is how we started on 9 Oct 2015, against SQLite:
+# 10 out of 1020 tests took 68s out of 657s
+# 15s for sandbox.ExecuteTests.test_pipeline_execute_C_twostep_pipeline_with_subpipeline
+# 8s for sandbox.ExecuteTests.test_pipeline_execute_B_twostep_pipeline_with_recycling
+# 8s for sandbox.FindSDTests.test_find_symds_subpipeline_input_and_intermediate
 # 7s for sandbox.ExecuteTests.test_pipeline_all_inputs_OK_nonraw
+# 7s for sandbox.ExecuteTests.test_pipeline_all_inputs_OK_raw
+# 7s for sandbox.ExecuteTests.test_pipeline_execute_A_simple_onestep_pipeline
+# 4s for archive.IsCompleteSuccessfulExecutionTests.test_runcomponent_unsuccessful_failed_integrity_check_during_recovery
+# 4s for sandbox.FindSDTests.test_find_symds_pipeline_input_and_intermediate_custom_wire
+# 4s for method.NonReusableMethodTests.test_execute_does_not_reuse
+# 4s for archive.RunTests.test_Run_clean_all_complete_RunOutputCables
 
 def parseOptions():
     parser = argparse.ArgumentParser()
