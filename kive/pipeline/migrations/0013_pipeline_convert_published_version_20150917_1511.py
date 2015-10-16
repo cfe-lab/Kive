d7bd37bd3f5db1a011e51ddfec3715ea2a6ae65f
Richard Liang <rliang@cfenet.ubc.ca>
2015-10-16 15:59:19 -0700

    Closes #437.

--- kive/pipeline/migrations/0013_pipeline_convert_published_version_20150917_1511.py
+++ kive/pipeline/migrations/0013_pipeline_convert_published_version_20150917_1511.py
@@ -19,6 +19,20 @@ def mark_published_versions(apps, schema_editor):
             published_version.save()
 
 
+def unmark_published_versions(apps, schema_editor):
+    """
+    Reversal of mark_published_versions.
+
+    This is a lossy operation, so in the case where multiple Pipelines are published
+    in a PipelineFamily, we publish the newest one.
+    """
+    PipelineFamily = apps.get_model("pipeline", "PipelineFamily")
+    for pf in PipelineFamily.objects.all():
+        all_published_versions = pf.members.filter(published=True).order_by("-revision_number")
+        pf.published_version = all_published_versions.first()
+        pf.save()
+
+
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -26,5 +40,5 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RunPython(mark_published_versions)
+        migrations.RunPython(mark_published_versions, unmark_published_versions)
     ]
