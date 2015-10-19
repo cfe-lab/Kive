6bde06502cfb02848b2d8dbe46de90eeca5239c6
don <donkirkby@gmail.com>
2015-10-19 10:11:31 -0700

    Close #314 by removing old-style AJAX methods, they were unused.

--- kive/kive/urls.py
+++ kive/kive/urls.py
@@ -99,9 +99,6 @@ urlpatterns = patterns(
     url(r'^run_pipeline$', 'sandbox.views.run_pipeline', name='run_pipeline'),
     url(r'^view_run/(?P<rtp_id>\d+)$', 'sandbox.views.view_run', name='view_run'),
     url(r'^view_run/(?P<rtp_id>\d+)/(?P<md5>[0-9a-fA-F]{32})$', 'sandbox.views.view_run', name='view_run'),
-    url(r'^filter_datasets$', 'sandbox.ajax.filter_datasets', name='filter_datasets'),
-    url(r'^filter_pipelines$', 'sandbox.ajax.filter_pipelines', name='filter_pipelines'),
-    url(r'^get_failed_output$', 'sandbox.ajax.get_failed_output', name='get_failed_output'),
 
     # Urls for django-rest-framework
     url(r'^api/', include(router.urls), name='api_home'),
