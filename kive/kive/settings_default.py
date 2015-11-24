6eeb9be7d72b6c9a84fcd9ce2486a281c739b501
Richard Liang <rliang@cfenet.ubc.ca>
2015-11-24 13:06:08 -0800

    Changed default KIVE_PROCESSING_GROUP to kiveprocessing.
    Also adding a little script I wrote to test some data migrations.

--- kive/kive/settings_default.py
+++ kive/kive/settings_default.py
@@ -255,7 +255,7 @@ KIVE_SANDBOX_WORKER_ACCOUNT = ""
 
 # The system group that contains both the user that launches the fleet and
 # the sandbox worker account.  This is ignored if KIVE_SANDBOX_WORKER_ACCOUNT is blank.
-KIVE_PROCESSING_GROUP = "kive"
+KIVE_PROCESSING_GROUP = "kiveprocessing"
 
 # Number of rows to display on the View Dataset page.
 DATASET_DISPLAY_MAX = 100
