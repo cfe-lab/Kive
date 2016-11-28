92a80153b10b13801072d6779acbba99f7e15e8a
James Nakagawa <jnakagawa@cfenet.ubc.ca>
2016-11-28 14:26:53 -0800

    Tests for drydock_contextmenu

--- kive/pipeline/tests.ts
+++ kive/pipeline/tests.ts
@@ -2,3 +2,4 @@ import './tests/canvas.tests';
 import './tests/dialog.tests';
 import './tests/pipeline_families.tests';
 import './tests/pipeline_functions.tests';
+import './tests/contextmenu.tests';
