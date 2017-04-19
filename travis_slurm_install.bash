2a1481b05ef789741e773102db7c7b48656e5408
Richard Liang <rliang@cfenet.ubc.ca>
2017-04-19 16:05:49 -0700

    Some fixes to the Travis build scripts.

--- travis_slurm_install.bash
+++ travis_slurm_install.bash
@@ -12,16 +12,21 @@ adduser --system slurm
 mkdir /var/run/slurm
 mkdir /var/log/slurm
 mkdir /var/lib/slurm
-
 chown slurm /var/run/slurm
 chown slurm /var/log/slurm
 chown slurm /var/lib/slurm
 
 mkdir /var/lib/slurm/slurmctld
 mkdir /var/lib/slurm/slurmd
-
 chown slurm /var/lib/slurm/slurmctld
 chown slurm /var/lib/slurm/slurmd
+
+touch /var/log/slurm/accounting
+chown slurm /var/log/slurm/accounting
+touch /var/log/slurm/job_completions
+chown slurm /var/log/slurm/job_completions
+touch /var/log/slurm/slurmctld.log
+chown slurm /var/log/slurm/slurmctld.log
 
 cp travis_slurm.conf /etc/slurm/slurm.conf
 
