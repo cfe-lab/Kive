95eb2e74d696345661620e41a488d6d1fb21decc
Richard Liang <rliang@cfenet.ubc.ca>
2017-04-25 13:23:16 -0700

    Singling out the test that failed on Travis so I can see more logging output.

---
+++ kive/kive/settings_test_pg_slurm_loud.py
@@ -0,0 +1,6 @@
+# This file is meant for use of the fleet while testing.
+
+from settings_test_pg_loud import *  # @UnusedWildImport
+
+# Enable the running of the Slurm tests.
+RUN_SLURM_TESTS = True
