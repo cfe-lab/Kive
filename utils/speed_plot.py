048e041b0fb41b3a101d171ff80f2baf6454ab0f
don <donkirkby@gmail.com>
2018-04-24 13:13:06 -0700

    Plot size and memory of Slurm jobs for #734.

--- utils/speed_plot.py
+++ utils/speed_plot.py
@@ -1,12 +1,16 @@
 import os
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 from collections import defaultdict
+from csv import DictReader, DictWriter
 from datetime import timedelta
 from itertools import islice, chain
 
 import seaborn as sns
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+import pandas as pd
 
+from kiveapi import KiveAPI
 from utils.speed_plot_loader import fetch_slurm, read_slurm, parse_date, read_optional_file
 
 
@@ -46,17 +50,43 @@ def parse_args():
     return parser.parse_args()
 
 
+def fetch_input_sizes(args, slurm_jobs):
+    data_path = 'speed_data_sizes.csv'
+    try:
+        with open(data_path) as f:
+            reader = DictReader(f)
+            return {int(row['run_id']): float(row['MB'])
+                    for row in reader}
+    except OSError:
+        pass
+    session = KiveAPI(args.kive_server)
+    session.login(args.kive_user, args.kive_password)
+    with open(data_path, 'w') as f:
+        writer = DictWriter(f, ['run_id', 'MB'])
+        writer.writeheader()
+        input_sizes = {}
+        for job in slurm_jobs:
+            run = session.get_run(job.run_id)
+            dataset = session.get_dataset(run.raw['inputs'][0]['dataset'])
+            dataset_size = dataset.raw['filesize'] / 1024 / 1024  # in MB
+            input_sizes[job.run_id] = dataset_size
+            writer.writerow({'run_id': job.run_id, 'MB': dataset_size})
+    return input_sizes
+
+
+def all_jobs(categorized_jobs):
+    for category_jobs in categorized_jobs.values():
+        for job in category_jobs:
+            yield job
+
+
 def plot_durations(slurm_jobs):
     categorized_jobs = defaultdict(list)
     for job in slurm_jobs:
         category = job.node
         categorized_jobs[category].append(job)
-    min_start = min(job.start
-                    for category_jobs in categorized_jobs.values()
-                    for job in category_jobs)
-    max_start = max(job.start
-                    for category_jobs in categorized_jobs.values()
-                    for job in category_jobs)
+    min_start = min(job.start for job in all_jobs(categorized_jobs))
+    max_start = max(job.start for job in all_jobs(categorized_jobs))
     categories = sorted(categorized_jobs)
     interval_size = timedelta(days=7)
     # min_start += interval_size  # First week only had 1 run.
@@ -91,6 +121,55 @@ def plot_durations(slurm_jobs):
     plt.show()
 
 
+def plot_size_and_memory(slurm_jobs, args):
+    df = pd.DataFrame([
+        {'job': job,
+         'memory (MB)': job.memory,
+         'job type': job.job_type,
+         'node': job.node,
+         'start': job.start,
+         'duration (hours)': job.duration.total_seconds()/3600.0}
+        for job in slurm_jobs])
+    input_sizes = fetch_input_sizes(args, df['job'])
+    df['size'] = [input_sizes[job.run_id] for job in df['job']]
+    limits = df.agg({'start': ['min', 'max']})
+    min_start = limits['start']['min']
+    interval_size = timedelta(days=7)
+    # min_start += interval_size  # First week only had 1 run.
+    df['interval'] = [int((job.start - min_start) / interval_size)
+                      for job in df['job']]
+    df = df.dropna(subset=['memory (MB)'])
+    df = df[df['duration (hours)'] > 31/3600.0]  # Checks memory every 30s
+    # df = df[df['job type'] == 'driver[prelim_map.py]']
+    df = df[~(df['job type'].isin(('bookkeeping', 'setup', 'Xcable',
+                                   'driver[trim_fastqs.py]',  #
+                                   'driver[sam2aln.py]',
+                                   'driver[remap.py]',  #
+                                   'Xdriver[prelim_map.py]',  #
+                                   'driver[filter_quality.py]',
+                                   'driver[fastq_g2p.py]',  #
+                                   'driver[coverage_plots.py]',
+                                   'driver[cascade_report.py]',
+                                   'driver[aln2counts.py]')))]
+    cmaps = ['Reds', 'Blues', 'Greens', 'Greys', 'Purples', 'Oranges']
+    grouped = df.groupby('job type')
+    label_patches = []
+    groups = grouped.groups
+    for group, group_jobs in groups.items():
+        group_size = len(group_jobs)
+        if group_size <= 2:
+            continue
+        cmap = cmaps[len(label_patches) % len(cmaps)]
+        x = df['size'][group_jobs]
+        y = df['memory (MB)'][group_jobs]
+        sns.kdeplot(x, y, cmap=cmap+'_d')
+        label_patch = mpatches.Patch(color=sns.color_palette(cmap)[2],
+                                     label='{} ({})'.format(group, group_size))
+        label_patches.append(label_patch)
+    plt.legend(handles=label_patches)
+    plt.show()
+
+
 def main(job_limit=None):
     args = parse_args()
     sns.set(color_codes=True)
@@ -98,14 +177,15 @@ def main(job_limit=None):
         fetch_slurm(args)
     old_slurm_file = read_optional_file('linux0_cluster_job_table.txt')
     with open(args.slurm_data, 'r') as f:
-        job_type_filter = 'driver[prelim_map.py]'
-        plot_durations(islice(
-            chain(read_slurm(old_slurm_file,
-                             args,
-                             job_type_filter=job_type_filter,
-                             is_dump=True),
-                  read_slurm(f, args, job_type_filter=job_type_filter)),
-            job_limit))
+        job_type_filter = None
+        plot_size_and_memory(
+            islice(chain(read_slurm(old_slurm_file,
+                                    args,
+                                    job_type_filter=job_type_filter,
+                                    is_dump=True),
+                         read_slurm(f, args, job_type_filter=job_type_filter)),
+                   job_limit),
+            args)
     print('Done.')
 
 
@@ -114,4 +194,4 @@ if __name__ == '__main__':
 elif __name__ == '__live_coding__':
     # noinspection PyUnresolvedReferences
     __live_coding_context__.message_limit = 100000
-    main(job_limit=1000)
+    main(job_limit=None)
