7f12f4f0c19755ffed501f160156ac57b27a806b
don <donkirkby@gmail.com>
2018-08-03 14:53:09 -0700

    Calculate MD5 and display file name for #737.

--- kive/container/views.py
+++ kive/container/views.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import os
+from io import BytesIO
+
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.urls import reverse
 from django.utils.decorators import method_decorator
@@ -9,6 +12,7 @@ from django.views.generic.edit import CreateView, UpdateView
 
 from container.forms import ContainerFamilyForm, ContainerForm, ContainerUpdateForm
 from container.models import ContainerFamily, Container
+from file_access_utils import compute_md5
 from portal.views import admin_check, developer_check
 
 decorators = [login_required, user_passes_test(developer_check)]
@@ -67,6 +71,23 @@ class ContainerCreate(CreateView):
     def form_valid(self, form):
         form.instance.user = self.request.user
         form.instance.family_id = self.kwargs['family_id']
+
+        # We need to get a file object to validate. We might have a path or we
+        # might have to read the data out of memory.
+        container_file = form.files['file']
+        if hasattr(container_file, 'temporary_file_path'):
+            with open(container_file.temporary_file_path()) as f:
+                md5 = compute_md5(f)
+        else:
+            if hasattr(container_file, 'read'):
+                f = BytesIO(container_file.read())
+            else:
+                f = BytesIO(container_file['content'])
+            md5 = compute_md5(f)
+            if hasattr(container_file, 'seek') and callable(container_file.seek):
+                container_file.seek(0)
+        form.instance.md5 = md5
+
         response = super(ContainerCreate, self).form_valid(form)
         self.object.grant_from_json(form.cleaned_data["permissions"])
         return response
@@ -109,6 +130,9 @@ class ContainerUpdate(UpdateView):
         context['family_id'] = self.object.family_id
         context['download_url'] = reverse('container-download',
                                           kwargs=dict(pk=self.object.pk))
+        context['file_name'] = self.object.file and os.path.relpath(
+            self.object.file.name,
+            Container.UPLOAD_DIR)
         return context
 
     def get_form_kwargs(self):
