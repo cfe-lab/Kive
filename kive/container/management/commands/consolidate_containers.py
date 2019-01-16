d4d66e05e2a979998a6a19489ceeff52f1c22668
Richard Liang <rliang@cfenet.ubc.ca>
2019-01-15 16:55:54 -0800

    Allowed specification of the directory in which to consolidate Container files.

--- kive/container/management/commands/consolidate_containers.py
+++ kive/container/management/commands/consolidate_containers.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 import logging
 import os
+from argparse import ArgumentDefaultsHelpFormatter
 
 from django.core.management.base import BaseCommand
 from django.conf import settings
@@ -16,8 +17,24 @@ logger = logging.getLogger(__name__)
 class Command(BaseCommand):
     help = "Consolidates Container file locations into their default directory."
 
-    def handle(self, *args, **options):
-        new_container_directory = os.path.normpath(os.path.join(settings.MEDIA_ROOT, Container.UPLOAD_DIR))
+    def add_arguments(self, parser):
+        parser.formatter_class = ArgumentDefaultsHelpFormatter
+
+        parser.add_argument('--directory',
+                            help='Directory to consolidate Container files in',
+                            default=os.path.join(settings.MEDIA_ROOT, Container.UPLOAD_DIR))
+
+    def handle(self, directory, **options):
+        self.consolidate_containers(directory=directory)
+
+    def consolidate_containers(self, directory):
+        """
+        Consolidate container files in the specified directory.
+
+        :param directory: an absolute path
+        :return:
+        """
+        new_container_directory = os.path.normpath(directory)
 
         # Scan through all Containers and examine the filenames.
         for container in Container.objects.all():
