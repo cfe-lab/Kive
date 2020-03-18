16991e5cbd0e82b495708fb374f6ee7bbed6b822
Nat Knight <nknight@cfenet.ubc.ca>
2020-03-18 14:06:33 -0700

    Restore munge installation on worker node

--- vagrant/bootstrap-worker.sh
+++ vagrant/bootstrap-worker.sh
@@ -31,18 +31,21 @@ mkdir --parents /etc/slurm
 cp  /usr/local/share/Kive/vagrant/slurm.conf \
     /usr/local/share/Kive/vagrant/cgroup.conf \
     /etc/slurm/
-# yum install -q -y epel-release
-# yum install -q -y munge munge-libs munge-devel rng-tools
-# rngd -r /dev/urandom
-# create-munge-key -r
-# systemctl enable munge
-# systemctl start munge
-yum groups install -q -y 'Development Tools'
+
 yum install -q -y epel-release
+
+yum groups install -q -y 'Development Tools'
+
+yum install -q -y munge munge-libs munge-devel rng-tools
+rngd -r /dev/urandom
+create-munge-key -r
+systemctl enable munge
+systemctl start munge
+
 yum install -q -y openssl openssl-devel pam-devel numactl numactl-devel \
     hwloc hwloc-devel lua lua-devel readline-devel rrdtool-devel \
     ncurses-devel man2html libibmad libibumad rpm-build perl-devel \
-    munge-libs munge-devel mariadb-devel
+    mariadb-devel
 wget -q https://download.schedmd.com/slurm/slurm-17.11.9-2.tar.bz2
 rpmbuild --quiet -ta slurm-17.11.9-2.tar.bz2
 yum install -q -y \
