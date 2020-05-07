9b92f0df6ff7041d765eb1772cf7c82024116015
Nat Knight <nknight@cfenet.ubc.ca>
2020-05-07 16:05:55 -0700

    Add extra DNF repos during machine setup

--- setupfiles/install-ansible.sh
+++ setupfiles/install-ansible.sh
@@ -2,5 +2,12 @@
 set -eu -o pipefail
 IFS=$'\t\n'
 
+# Enable extra repositories
+dnf install -q -y epel-release
+dnf config-manager --set-enabled PowerTools
+
+# Install Python3
 dnf install -q -y python3
+
+# Install Python packages
 python3 -m pip install -r /vagrant/requirements.txt
