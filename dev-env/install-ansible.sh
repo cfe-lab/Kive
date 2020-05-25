50ac660565364a057501d35128cd5ca262d4ab15
Nat Knight <nknight@cfenet.ubc.ca>
2020-05-25 14:11:24 -0700

    Add code for a development environment
    This commit:
    
    - Factors the Ansible roles created in the `cluster-setup` research project
      into their own top-level directory so that they can be shared between environments
    - Adds a `dev-env` environment that uses the Ansible roles.
    
    The environment includes a Vagrantfile for provisioning a suitable test host
    (with Kive's source code mapped). It runs all of the components (head and
    worker nodes for Kive and Slurm) in a single VM to reduce the memory and CPU
    requirements for running it.
    
    In addition to setting up Kive, the development environment includes a number of ergonimic features that aren't present in production:
    
    - Automaticaly activate the Kive virtualenv and source environment variables
    - Add the default `vagrant` user to the necessary groups
    - Allow traffic on port 8080 (the default Kive development port)

---
+++ dev-env/install-ansible.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -eu -o pipefail
+IFS=$'\t\n'
+
+# Enable extra repositories
+dnf install -q -y epel-release
+dnf config-manager --set-enabled PowerTools
+
+# Install Python3
+dnf install -q -y python3
+
+# Install Python packages
+python3 -m pip install -r /vagrant/requirements.txt
