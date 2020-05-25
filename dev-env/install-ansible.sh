b96c0e1a180d80b5a30f83cade7619f7bae28b6a
Nat Knight <nknight@cfenet.ubc.ca>
2020-05-25 14:56:18 -0700

    Minor ergonomic fixes

--- dev-env/install-ansible.sh
+++ dev-env/install-ansible.sh
@@ -11,3 +11,6 @@ dnf install -q -y python3
 
 # Install Python packages
 python3 -m pip install -r /vagrant/requirements.txt
+
+# Add host keys to `known_hosts`
+ssh-keyscan head >> ~/.ssh/known_hosts
