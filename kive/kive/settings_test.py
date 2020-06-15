6bca131de4adc5fe360c7155bd88aa3cad2ea4be
Nat Knight <nknight@cfenet.ubc.ca>
2020-06-15 11:22:41 -0700

    Remove unused settings file.
    Closes #952

--- kive/kive/settings_test.py
+++
@@ -1,9 +0,0 @@
-# This file overrides some of the defaults to make tests run faster.
-# Use it by running ./manage.py test --settings=kive.test_settings
-
-from settings_test_pg import *  # @UnusedWildImport
-
-# Run with an in-memory database: about twice as fast as PostgreSQL
-DATABASES['default'] = {'ENGINE': 'django.db.backends.sqlite3',
-                        'TEST': {'NAME': ':memory:'},
-                        'NAME': 'kive.db'}
