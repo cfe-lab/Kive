a9c8c3d4372491c2c7c613de4bb967ba04fd7783
Nat Knight <nknight@cfenet.ubc.ca>
2020-06-22 17:11:44 -0700

    WIP Converted an API example

--- api/example.py
+++ api/example.py
@@ -1,77 +1,75 @@
-from pprint import pprint
 import time
 
-from kiveapi import KiveAPI
-
-RULE = " " + 66 * "-"
-ACTIVE_STATES = "NRLS"
-
+import kiveapi
 
 # Use HTTPS on a real server, so your password is encrypted.
-KiveAPI.SERVER_URL = 'http://localhost:8080'
 # Don't put your real password in source code, store it in a text file
 # that is only readable by your user account or some more secure storage.
-session = KiveAPI()
+session = kiveapi.KiveAPI("http://localhost:8080")
 session.login('kive', 'kive')
 
 # Get the data by ID
 dataset1 = session.get_dataset(1)
-dataset2 = session.get_dataset(2)
-
-# or get the data by name
-dataset1 = session.find_datasets(name='example_pairs.csv_20200615170532257697')[0]
-dataset2 = session.find_datasets(name='example_names.csv_20200615170519866442')[0]
-
+# or get the data by name.
+example_names = session.find_datasets(name="example_names.csv")[0]
 
 # Get app information
 app = session.endpoints.containerapps.get(1)
+# and app arguments information.
 appargs = session.endpoints.containerapps.get(f"{app['id']}/argument_list")
-print("App" + RULE)
-pprint(app)
-print("App Args" + RULE)
-pprint(appargs)
 
-
-# Start a pipeline run
+# Start a run of the app, giving the dataset as an argument.
 inputarg = next(a for a in appargs if a['type'] == 'I')
-kiveuser = session.endpoints.users.get(1)
-dataset = session.endpoints.datasets.get(1)
 runspec = {
     "name": "example",
-
     "app": app["url"],
     "datasets": [
         {
             "argument": inputarg["url"],
-            "dataset": dataset["url"],
+            "dataset": example_names.raw["url"],
         },
     ],
 }
+print("Starting example run... ", end="")
 created_containerrun = session.endpoints.containerruns.post(json=runspec)
-print("Created a Run" + RULE)
-pprint(created_containerrun)
 
-
-# Monitor for completion
-runid = created_containerrun["id"]
+# Monitor the run for completion
+ACTIVE_STATES = "NSLR"
+INTERVAL = 1.0
+MAX_WAIT = 300.0
 starttime = time.time()
-while True:
+elapsed = 0.0
+
+runid = created_containerrun["id"]
+print(f"Waiting for run {runid} to finish.")
+while elapsed < MAX_WAIT:
     containerrun = session.endpoints.containerruns.get(runid)
     state = containerrun["state"]
+    elapsed = round(time.time() - starttime, 2)
     if state in ACTIVE_STATES:
-        elapsed = round(time.time() - starttime, 2)
-        print(f"Run in progress (state={state}, {elapsed}s elapsed))\r", end="")
-        time.sleep(4)
+        print(f"Run in progress (state={state}, {elapsed}s elapsed)")
+        time.sleep(INTERVAL)
     elif state == "C":
-        print("Run finished" + RULE)
-        pprint(containerrun)
+        print(f"Run {runid} finished after {elapsed}s; fetching results")
         break
     else:
-        print("Run failed" + RULE)
-        pprint(containerrun)
+        import pprint
+        print(f"Run {runid} failed after {elapsed}s; exiting")
+        pprint.pprint(containerrun)
         exit(1)
-
+else:
+    print("Example timed out")
+    exit(1)
 
 # Retrieve the output and save to a file
+run_datasets = session.get(containerrun["dataset_list"]).json()
+for run_dataset in run_datasets:
+    if run_dataset.get("argument_type") == "O":
+        dataset = session.get(run_dataset["dataset"]).json()
+        filename = dataset["name"]
+        with open(filename, "wb") as outf:
+            print(f"  downloading {filename}...", end="")
+            session.download_file(outf, dataset["download_url"])
+        print(" done.")
 
-# TODO(nknight)
+print("Example run finished.")
