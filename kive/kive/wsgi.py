c45e9569a65a6a5c86e8d3733bd711ec32ef8b77
Nathaniel Knight <nknight@cfenet.ubc.ca>
2020-08-12 12:59:04 -0700

    Revert change in import/config order
    This commit reverts a change that might cause problems with creating
    Kive's WSGI Application object. Because Django must perform setup before
    creating the object, any imports that depend on the
    `DJANGO_SETTINGS_MODULE` variable might behave differently with the
    changes that this commit reverts. The source of `get_wsgi_application`
    looks like it should defer any relevant loading until after the variable
    is set, but the change is easy to revert and has very limited scope.

--- kive/kive/wsgi.py
+++ kive/kive/wsgi.py
@@ -14,7 +14,6 @@ framework.
 
 """
 import os
-from django.core.wsgi import get_wsgi_application
 
 
 # We defer to a DJANGO_SETTINGS_MODULE already in the environment. This breaks
@@ -26,6 +25,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kive.settings")
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
+from django.core.wsgi import get_wsgi_application  # noqa
 application = get_wsgi_application()
 
 # Apply WSGI middleware here.
