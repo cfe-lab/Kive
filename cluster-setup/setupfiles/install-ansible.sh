0cc0787a6f6004dd14ba3fcd21d841bb2c1f1b60
Richard Liang <rliang@cfenet.ubc.ca>
2023-07-12 16:11:20 -0700

    Attempting to convert the Vagrant configuration to use Ubuntu instead of CentOS.

--- cluster-setup/setupfiles/install-ansible.sh
+++ cluster-setup/setupfiles/install-ansible.sh
@@ -2,12 +2,13 @@
 set -eu -o pipefail
 IFS=$'\t\n'
 
-# Enable extra repositories
-dnf install -q -y epel-release
-dnf config-manager --set-enabled PowerTools
+# # Enable extra repositories
+# dnf install -q -y epel-release
+# dnf config-manager --set-enabled PowerTools
 
 # Install Python3
-dnf install -q -y python3
+# dnf install -q -y python3
+apt install -y python3
 
 # Install Python packages
 python3 -m pip install -r /vagrant/requirements.txt
