4b26c343c9669b9b96461d8887ab8fa3305efb17
Vitaliy Mysak <vmysak@bccfe.ca>
2026-06-15 22:46:02 +0000

    Revert "Adjust provisioning timeout"
    This reverts commit 9ee61daf7f38a875c6be70f97cbac0e379de56bd.

--- utils/kivedevel/kivedevel/build_vm/provision.py
+++ utils/kivedevel/kivedevel/build_vm/provision.py
@@ -26,7 +26,7 @@ def maybe_provision_instance(
         return
 
     logger.info("Waiting for %s instance %s to finish cloud-init provisioning...", instance_type, instance)
-    deadline = time.time() + 60 * 60 * 3 # 3 hours
+    deadline = time.time() + 1200
     while time.time() < deadline:
         done, _ = _pull_file(cmds, instance, "/run/kive-provision.done")
         if done:
