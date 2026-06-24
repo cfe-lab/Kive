07053546aead48b5de9c823e4f65975ac8fd0677
Vitaliy Mysak <vmysak@bccfe.ca>
2026-06-24 00:14:20 +0000

    Revert "fix: change local dev build image from noble to jammy"
    This reverts commit 57c93cbf39d3ee59102f926b0c915eda300dfc37.

--- utils/kivedevel/kivedevel/build_vm/instance.py
+++ utils/kivedevel/kivedevel/build_vm/instance.py
@@ -17,7 +17,7 @@ def ensure_instance(cmds: Cmds, instance: str, instance_type: str, profile: str,
         logger.info("Creating %s instance %s...", instance_type, instance)
         create_args = [
             "create",
-            "images:ubuntu/jammy/cloud",
+            "images:ubuntu/noble/cloud",
             instance,
             "--config",
             f"limits.cpu={cpu}",
