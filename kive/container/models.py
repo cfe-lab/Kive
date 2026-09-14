de87c8046e3976228a779b0a4928e17c8c82cac1
Donaim <15235025+Donaim@users.noreply.github.com>
2026-09-14 16:23:16 -0700

    Update kive/container/models.py
    Co-authored-by: rhliang <rliang@bccfe.ca>

--- kive/container/models.py
+++ kive/container/models.py
@@ -1129,7 +1129,7 @@ def _slurm_failure_diagnostic(run_id, slurm_job_id, records):
             lines.extend([
                 '',
                 _format_slurm_accounting(
-                    'Slurm step {}'.format(step_id), records[step_id]),
+                    f'Slurm step {step_id}', records[step_id]),
             ])
     return '\n'.join(lines)
 
